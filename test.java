9417384e25ed9ff8d57089a97097774edaedf23c
acer42 <qduong@students.42wolfsburg.de>
2023-12-09 20:41:39 +0100

    test commit

---
+++ test.java
@@ -0,0 +1,9 @@
+import edu.duke.*;
+
+public class test{
+	public static void main(String[]args){
+		FileResource fr = new FileResource();
+		String all = fr.asString();
+		System.out.println(all);
+	} 
+}
